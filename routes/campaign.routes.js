23c2440bfba447cd007c907e2bf63068fdce9cf3
Barbara Sanser <bsanser@gmail.com>
2018-04-07 17:57:54 +0200

    feat: secure middleware

--- routes/campaign.routes.js
+++ routes/campaign.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const campaignController = require('../controllers/campaign.controller');
 
-router.post('/', campaignController.create);
+const secureMiddleware = require('../middlewares/secure.middleware');
+
+router.post('/', secureMiddleware.isAuthenticated, campaignController.create);
 router.get('/', campaignController.list);
 router.get('/:id', campaignController.get);
-router.put('/:id', campaignController.edit);
-router.delete('/:id', campaignController.delete);
-router.put('/:id/follow', campaignController.follow);
+router.put('/:id', secureMiddleware.isAuthenticated, campaignController.edit);
+router.delete('/:id', secureMiddleware.isAuthenticated, campaignController.delete);
+router.put('/:id/follow', secureMiddleware.isAuthenticated, campaignController.follow);
 
 module.exports = router;
--- routes/session.routes.js
+++ routes/session.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const sessionController = require('../controllers/session.controller');
 
+const secureMiddleware = require('../middlewares/secure.middleware');
+
 
 router.post('/', sessionController.create);
-router.delete('/', sessionController.destroy);
+router.delete('/', secureMiddleware.isAuthenticated, sessionController.destroy);
 
 module.exports = router;
